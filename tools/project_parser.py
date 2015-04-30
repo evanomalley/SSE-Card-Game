571ad11e11f2a8fe348d12a99937807722d18766
Daniel <dds2130@rit.edu>
2015-04-29 22:42:49 -0400

    Added projects

--- tools/project_parser.py
+++ tools/project_parser.py
@@ -1,16 +1,16 @@
 """
-Parses spreadsheet -> json
+Parses tab seperated values -> json
 Kocsen Chung
-Dan Santoro
+Daniel Santoro
 Winning
 """
 import re
 import json
 from json import JSONEncoder
 
-input_file = "acards.tsv"
-output_file = '../action.json'
-img_folder = 'pictures'
+input_file = "pcards.tsv"
+output_file = '../project.json'
+img_folder = 'projects'
 img_extension = '.png'
 
 class CardEncoder(JSONEncoder):
@@ -34,13 +34,20 @@ class Card:
         self.name = name
         self.img = img_path
         self.flavorText = flavor_text
-        self.stats = [description]
+        self.stats = description
 
-def parse_subtype(subtype):
-    if (subtype == 'Normal'):
-        return ''
-    else:
-        return subtype
+def parse_stats(platform, size):
+    commit = 0;
+    if size == 'Small':
+        commit = 10
+    elif size == 'Medium':
+        commit = 12
+    elif size == 'Large':
+        commit = 18
+
+    if platform == 'None':
+        platform = 'Generic'
+    return ['Type: ' + platform, 'Commits Required: ' + str(commit)]
 
 def main():
     with open(input_file) as f:
@@ -50,23 +57,24 @@ def main():
     for line in file_lines:
         split = line.strip().split("\t")
         # print(split)
-        if len(split) <= 4:
+
+        if len(split) <= 2:
             continue
 
         try:
-            num_copies = int(split[0])
+            num_copies = int(split[1])
         except ValueError:
             continue
 
-        _type = "action"
-        name = split[1]
-        subtype = parse_subtype(split[2])
-        # ignore split[3]
-        description = split[4]
+        _type = "project"
+        subtype = ''
+        name = split[2]
         try:
-            flavor_text = split[5]
+            flavor_text = split[3]
         except IndexError:
             flavor_text = ""
+
+        description = parse_stats(split[4], split[5])
 
         img_path = img_folder + '/' + re.sub(r'\s+','',name) + img_extension
 
