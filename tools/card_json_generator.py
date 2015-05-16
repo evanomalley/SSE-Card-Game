79f4522522e1ac3fd5758d2b379d0e682a2dc109
Daniel <dds2130@rit.edu>
2015-05-15 20:02:07 -0400

    Implemented Project Parser, doesn't generate ability text

--- tools/card_json_generator.py
+++ tools/card_json_generator.py
@@ -1,8 +1,8 @@
-"""
+'''
 Parses spreedsheet tsv or csv to json
 
 Daniel Santoro
-"""
+'''
 
 import sys, getopt
 import re
@@ -10,14 +10,16 @@ import json
 from json import JSONEncoder
 
 #An array of all the files to parse
-input_files = ["pcards.tsv"]
+#Overridden if command line argument is used
+input_files = ['projects.tsv']
 #Variables for customization
 img_extension = '.png'
 img_folder = 'pictures'
 #Output path
-project_file = '../project.json'
-action_file = '../action.json'
-student_file = '../student.json'
+output_path = '../'
+
+#Global variable for file type
+file_type = ''
 
 
 class CardEncoder(JSONEncoder):
@@ -26,7 +28,7 @@ class CardEncoder(JSONEncoder):
 
 class Card:
 
-    def __init__(self, num_copies, _type, subtype, name, img_path, flavor_text, description):
+    def __init__(self, num_copies, name, _type, subtype, abilities, flavor_text, img_path):
         copies_len = []
         for i in range(num_copies):
             copies_len.append(i)
@@ -37,11 +39,12 @@ class Card:
         self.name = name
         self.img_path = img_path
         self.flavorText = flavor_text
-        self.stats = description
+        self.abilities = abilities
 
-def file_write(output_file):
+def file_write(output_name, json_array):
+    output_file = output_path + output_name
     with open( output_file, 'w') as jsonfile:
-        json.dump(json_ary, jsonfile, cls=CardEncoder, indent=2)
+        json.dump(json_array, jsonfile, cls=CardEncoder, indent=2)
 
 class DataParser:
 
@@ -54,57 +57,121 @@ class DataParser:
 class ProjectParser(DataParser):
     
     def parse(self, file_lines):
-        json_aray = []
-        #TODO
-        header = get_line_values(file_lines[0])
 
+        header_dict = self.assign_header(get_line_values(file_lines[0]))
+
+        json_array = []
+
+        #Constants for project cards
+        _type = 'project'
+        subtype = ''
+
+        #For each row, parse the values and create json objects of the cards
+        for line in file_lines[1:]:
+            row_split = get_line_values(line)
+
+            #Make sure each row is as long as the header
+            #Prevents index out of bounds
+            while len(row_split) < len(header_dict):
+                    row_split.append('')
+
+            name = row_split[header_dict[_type + ' name']]
+
+            #if no value in the name column, skip row
+            if not name:
+                continue
+
+            #Try to assign the number of copies of the card, else skip row
+            try:
+                num_copies = int(row_split[header_dict['#']])
+            except ValueError:
+                continue
+
+            abilities = self.abilities_text(row_split[header_dict['platform']], row_split[header_dict['size']])
+
+            flavor_text = row_split[header_dict['flavor text']]
+
+            img_path = img_folder + '/'+ _type + 's/' + re.sub(r'\s+','',name) + img_extension
+
+            #Append a new card object to the array
+            json_array.append(Card(num_copies, name, _type, subtype, abilities, flavor_text, img_path))
+
+        file_write(_type + '.json', json_array)
+
+    #Get header data, map header to index
+    def assign_header(self, header):
         header_dict = {}
 
-        #Get header data, map header to index
         for idx, col in enumerate(header):
-            if col == "#":
-                header_dict["#"] = idx
-            elif col == "Project Name":
-                header_dict["Project Name"] = idx
-            elif col == "Flavor Text":
-                header_dict["Flavor Text"] = idx
-            elif col == "Platform":
-                header_dict["Platform"] = idx
-            elif col == "Size":
-                header_dict["Size"] = idx
 
+            #Work around lowering case
+            compare = col
+            compare = compare.lower()
+
+            if compare == '#':
+                header_dict['#'] = idx
+            elif compare == 'project name':
+                header_dict['project name'] = idx
+            elif compare == 'flavor text':
+                header_dict['flavor text'] = idx
+            elif compare == 'platform':
+                header_dict['platform'] = idx
+            elif compare == 'size':
+                header_dict['size'] = idx
+
+        return header_dict
+
+    def abilities_text(self, platform, size):
+        commit = 0;
+        pp = 0;
+
+        size.lower()
+        if size == 'small':
+            pp = 3;
+            commit = 10
+        elif size == 'medium':
+            pp = 5;
+            commit = 12
+        elif size == 'large':
+            pp = 8;
+            commit = 18
+
+        if platform == 'none':
+            platform = 'Generic'
+        return ['Type: ' + size + ', ' + platform,  
+        'Project Points: ' + str(pp),
+        'Story Points: ' + str(commit)]
 
 
 class ActionParser(DataParser):
 
     def parse(self, file_lines):
-        json_aray = []
+        json_array = []
         #TODO
 
 class StudentParser(DataParser):
 
     def parse(self, file_lines):
-        json_aray = []
+        json_array = []
         #TODO
 
 def get_line_values(line):
 
     row = []
 
-    if f.endswith('.tsv'):
-        row = (line.strip().split("\t"))
-    elif f.endswith('.csv'):
-        row = (lin.strip().split(","))
+    if file_type == 'tsv':
+        row = (line.strip().split('\t'))
+    elif file_type == "csv":
+        row = (lin.strip().split(','))
 
     return row
 
 
 def main(argv):
-    #TODO
-    json_aray = []
 
+    #Try command line arguments
     try:
-        opts, args = getopt.getopt(argv, "hi:", ["help", "input="])
+        opts, args = getopt.getopt(argv, 'hi:', ['help', 'input='])
     except getopt.GetoptError:
         usage()
         sys.exit(2)
@@ -113,7 +180,7 @@ def main(argv):
         if opt == '-h':
             usage()
             sys.exit()
-        elif opt in ("-i", "--input"):
+        elif opt in ('-i', '--input'):
             global input_files 
             input_files = [arg]
 
@@ -121,9 +188,20 @@ def main(argv):
         with open(input_file) as f:
             file_lines = f.readlines()
 
+            global file_type
+            if input_file.endswith('.tsv'):
+                file_type = 'tsv'
+            elif input_file.endswith('.csv'):
+                file_type = 'csv'
+
+            #TODO Call the parsers
+            #split first line (header) and look for the 'name' row
+            parser = ProjectParser()
+            parser.parse(file_lines)
+
 #Prints the usage of the command line arguments
 def usage():
     print 'usage: -i <inputfile>'
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main(sys.argv[1:])
